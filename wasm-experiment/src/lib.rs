6e123c75b50710e34d62e3b465e0cae1a92cc430
Simon <simonwoodburyforget@gmail.com>
2019-12-07 08:17:20 -0500

    Working Wasm sample.

--- wasm-experiment/src/lib.rs
+++ wasm-experiment/src/lib.rs
@@ -1,3 +1,11 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> { Ok(()) }
+
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 { a + b }
+
 #[cfg(test)]
 mod tests {
     #[test]
