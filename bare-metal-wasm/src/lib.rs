dfa4534eb08ba02e27539167f846655f14674f0e
Simon <simonwoodburyforget@gmail.com>
2020-01-02 14:30:31 -0500

    Procedurally generated texture.

--- bare-metal-wasm/src/lib.rs
+++ bare-metal-wasm/src/lib.rs
@@ -9,3 +9,26 @@ fn handle_panic(_: &core::panic::PanicInfo) -> ! {
 pub extern "C" fn the_answer() -> u32 {
     42
 }
+
+const WIDTH: usize = 600;
+const HEIGHT: usize = 600;
+
+#[no_mangle]
+static mut BUFFER: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
+
+#[no_mangle]
+pub unsafe extern "C" fn go() {
+    // This is called from JavaScript, and should *only* be
+    // called from JavaScript. If you maintain that condition,
+    // then we know that the &mut we're about to produce is
+    // unique, and therefore safe.
+    render_frame_safe(&mut BUFFER)
+}
+
+fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            buffer[y * WIDTH + x] = (x ^ y) as u32 | 0xFF_00_00_00;
+        }
+    }
+}
