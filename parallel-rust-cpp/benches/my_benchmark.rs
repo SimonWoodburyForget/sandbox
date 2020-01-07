0063b4bd1a54f6ef4b262164ebb3257077ff69ba
Simon <simonwoodburyforget@gmail.com>
2020-01-07 12:51:03 -0500

    More v0 benchmarks.

--- parallel-rust-cpp/benches/my_benchmark.rs
+++ parallel-rust-cpp/benches/my_benchmark.rs
@@ -5,17 +5,47 @@ use rand::prelude::*;
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("step");
     group.sample_size(30);
-    for size in 1..14 {
+    for size in 8..14 {
         let n: usize = (size + 10) * 4;
         let d: Vec<f32> = (0..).map(|_| rand::random()).take(n * n).collect();
         group.throughput(Throughput::Bytes(d.len() as u64));
-        group.bench_function(format!("step input size {}", d.len()), |b| {
+
+        group.bench_with_input(BenchmarkId::new("v0a", d.len()), &d, |b, d| {
             let mut r: Vec<f32> = vec![0.0; n * n];
-            b.iter(|| step(&mut r, &d, n))
+            b.iter(|| step_v0a(&mut r, d, n))
         });
+        group.bench_with_input(BenchmarkId::new("v0b", d.len()), &d, |b, d| {
+            let mut r: Vec<f32> = vec![0.0; n * n];
+            b.iter(|| step_v0b(&mut r, d, n))
+        });
+        group.bench_with_input(BenchmarkId::new("v0c", d.len()), &d, |b, d| {
+            let mut r: Vec<f32> = vec![0.0; n * n];
+            b.iter(|| step_v0c(&mut r, d, n))
+        });
+
+        // group.bench_function(format!("step input size {}", d.len()), |b| {
+
+        //     b.iter(|| step(&mut r, &d, n))
+        // });
     }
     group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
+
+// fn bench_fibs(c: &mut Criterion) {
+//     let mut group = c.benchmark_group("Fibonacci");
+//     for i in [20u64, 21, 22, 23, 24].iter() {
+//         // group.bench_with_input(BenchmarkId::new("Recursive", i), i, |b, i| {
+//         //     b.iter(|| fibonacci_slow(*i))
+//         // });
+//         group.bench_with_input(BenchmarkId::new("Iterative", i), i, |b, i| {
+//             b.iter(|| fibonacci_fast(*i))
+//         });
+//     }
+//     group.finish();
+// }
+
+// criterion_group!(benches, bench_fibs);
+
 criterion_main!(benches);
--- parallel-rust-cpp/src/lib.rs
+++ parallel-rust-cpp/src/lib.rs
@@ -2,7 +2,41 @@ extern crate rayon;
 use rayon::prelude::*;
 
 #[inline]
-pub fn step(r: &mut [f32], d: &[f32], n: usize) {
+pub fn step_v0a(r: &mut [f32], d: &[f32], n: usize) {
+    for i in 0..n {
+        for j in 0..n {
+            let mut v = std::f32::INFINITY;
+            for k in 0..n {
+                let x = d[n * i + k];
+                let y = d[n * k + j];
+                let z = x + y;
+                v = v.min(z);
+            }
+            r[n * i + j] = v;
+        }
+    }
+}
+
+#[inline]
+pub fn step_v0b(r: &mut [f32], d: &[f32], n: usize) {
+    let step_row = |(i, r_row): (usize, &mut [f32])| {
+        for (j, res) in r_row.iter_mut().enumerate() {
+            let mut v = std::f32::INFINITY;
+            for k in 0..n {
+                let x = d[n * i + k];
+                let y = d[n * k + j];
+                let z = x + y;
+                v = v.min(z);
+            }
+            *res = v;
+        }
+    };
+    // r.par_chunks_mut(n).enumerate().for_each(step_row);
+    r.chunks_mut(n).enumerate().for_each(step_row);
+}
+
+#[inline]
+pub fn step_v0c(r: &mut [f32], d: &[f32], n: usize) {
     let step_row = |(i, r_row): (usize, &mut [f32])| {
         for (j, res) in r_row.iter_mut().enumerate() {
             let mut v = std::f32::INFINITY;
@@ -16,7 +50,7 @@ pub fn step(r: &mut [f32], d: &[f32], n: usize) {
         }
     };
     r.par_chunks_mut(n).enumerate().for_each(step_row);
-    r.chunks_mut(n).enumerate().for_each(step_row);
+    // r.chunks_mut(n).enumerate().for_each(step_row);
 }
 
 #[inline]
@@ -25,5 +59,30 @@ pub fn fibonacci(n: u64) -> u64 {
         0 => 1,
         1 => 1,
         n => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+pub fn fibonacci_slow(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci_slow(n - 1) + fibonacci_slow(n - 2),
+    }
+}
+
+pub fn fibonacci_fast(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
     }
 }
